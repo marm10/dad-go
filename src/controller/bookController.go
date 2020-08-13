c2c4ad0cb4a5819d79db755854bde60dfc75d2d5
marm10 <marcilio.ec.ufc@gmail.com>
2020-08-12 23:11:55 -0300

    new commit

--- src/controller/bookController.go
+++ src/controller/bookController.go
@@ -1,13 +1,27 @@
 package controller
 
 import (
+	"fmt"
 	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 	h "ufc.com/deti/go-dad/src/handlerException"
-	b "ufc.com/deti/go-dad/src/model"
+	b "ufc.com/deti/go-dad/src/model"	
+
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	BUCKET_NAME = "book-covers"
+	REGION = "us-east-2"
+)
+
+var (
+	s3session *s3.s3
 )
 
 func GetAll(w http.ResponseWriter, r *http.Request) {
@@ -64,4 +78,41 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusAccepted)
 
+}
+
+func CreateBucket(w http.ResponseWriter, r *http.Request) () {
+	w.Header().Set("Content-Type", "application/json")
+
+	att := mux.Vars(r)
+	name := att["name"]
+	name, _ := strconv.Atoi(name)
+
+	InitS3();
+	resp, err := s3session.ListBuckets(&s3.ListBucketsInput{
+		Bucket: aws.String(name),
+		CreateBucketConfiguration: &s3.CreateBucketConfiguration{
+			LocationConstraint: aws.String(REGION),
+		},
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeBucketAlreadyExists:
+				fmt.Println("Bucket name already in use!")
+				panic(err)
+			case s3.ErrCodeBucketAlreadyOwnedByYou:
+				fmt.Println("Bucket exists and is owned by you!")
+			default:
+				panic(err)
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusAccepted)	
+}
+
+func InitS3() {
+	s3session = s3.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(REGION)
+	})))
 }
