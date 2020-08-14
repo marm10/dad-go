bac388bf5cc1d7e9dfda2fbe5792354d2aaf9c4f
marm10 <marcilio.ec.ufc@gmail.com>
2020-08-13 23:12:20 -0300

    upload file

--- src/controller/bookController.go
+++ src/controller/bookController.go
@@ -196,7 +196,7 @@ func GetObject(fileName string, bucketName string) (obj []byte){
 		panic(err)
 	}
 
-	obj, err := ioutil.ReadAll(resp.Body)
+	obj, _ := ioutil.ReadAll(resp.Body)
 
 	return obj
 }
